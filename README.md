ff17faae33fba0124e73872bbd496ba4a905d179
SilverofLight <a2061686759@outlook.com>
2024-12-19 12:50:19 +0800

    fix bug

---
+++ README.md
@@ -0,0 +1,9 @@
+# 介绍
+
+> 这是一个几乎完全由 Cursor 写的nvim翻译插件
+
+![pic](./src/1734583312.png) 
+
+使用的后端来自[kd](https://github.com/Karmenzind/kd)
+
+## 使用lazy配置：
--- plugin/kd.lua
+++ plugin/kd.lua
@@ -3,5 +3,5 @@ local api, fn = vim.api, vim.fn
 local kd = require 'kd'
 local command = api.nvim_create_user_command
 
-command('translate', function() kd.translate() end,
+command('Translate', function() kd.translate() end,
     { desc = 'Translate selected word' })
