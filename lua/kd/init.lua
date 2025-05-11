fbebf5a2983fb1472d95e79b9b4ffcaf99b7e870
SilverofLight <a2061686759@outlook.com>
2025-05-11 19:22:30 +0800

    fix(visual): fix bug in visual mode
    '< & '> 在退出视觉模式之后才会更新，应该替换为可以实时更新的 v & .

--- lua/kd/init.lua
+++ lua/kd/init.lua
@@ -52,10 +52,15 @@ local cursor_win = nil
 local function get_visual_selection()
 	-- 获取可视模式类型
 	-- 获取选区的起始和结束位置
-	local start_pos = vim.fn.getpos("'<")
-	local end_pos = vim.fn.getpos("'>")
+	local start_pos = vim.fn.getpos("v")
+	local end_pos = vim.fn.getpos(".")
 	local start_row, start_col = start_pos[2], start_pos[3]
 	local end_row, end_col = end_pos[2], end_pos[3]
+  if start_col > end_col then
+    local tmp = end_col
+    end_col = start_col
+    start_col = tmp
+  end
 
 	-- 检查是否跨行
 	if start_row == end_row then
